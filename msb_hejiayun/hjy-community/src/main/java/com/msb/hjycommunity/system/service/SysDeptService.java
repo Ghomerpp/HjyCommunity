75c24ec8b2342c406e360923ee2949a69e916cc4
Ghomerpp <148603444+Ghomerpp@users.noreply.github.com>
2026-03-01 15:52:09 +0800

    Delete msb_hejiayun directory

--- msb_hejiayun/hjy-community/src/main/java/com/msb/hjycommunity/system/service/SysDeptService.java
+++
@@ -1,9 +0,0 @@
-package com.msb.hjycommunity.system.service;
-
-import com.msb.hjycommunity.system.domain.SysDept;
-
-import java.util.List;
-
-public interface SysDeptService {
-    public List<SysDept> selectDeptList(SysDept sysDept);
-}
